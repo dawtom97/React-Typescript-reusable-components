457fca6dd5885fe9b048b78bbe36d3b44adbcd6c
Dawid <daw.tomczyk1997@gmail.com>
2022-07-02 09:00:55 +0200

    paragraph component with bold and regular weight options and optional custom font size

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { Button } from './components/atoms/Button/Button';
 import { Heading } from './components/atoms/Heading/Heading';
 import { Input } from './components/atoms/Input/Input';
+import { Paragraph } from './components/atoms/Paragraph/Paragraph';
 
 const App: React.FC = () => {
   const focusEl = useRef();
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <Heading level='h1'>cyrk</Heading>
       <Heading isThin isUpperCase level='h1'>cyrk</Heading>
       <Heading level='h3'>cyrk</Heading>
+      <Paragraph>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus ad maxime suscipit, quasi nemo, culpa iure pariatur unde quos autem fuga consequatur amet! Optio aliquam iusto rerum voluptas. Sequi, eius!</Paragraph>
       <Button ariaLabel='Buy now button' color='primary' size='small'>
         Kup teraz
       </Button>
