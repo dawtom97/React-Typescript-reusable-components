971ad93a4a92fd6771d13834b27de49f7e0accbd
Dawid <daw.tomczyk1997@gmail.com>
2022-07-03 16:50:40 +0200

    heading level test

--- src/components/atoms/Heading/Heading.test.tsx
+++ src/components/atoms/Heading/Heading.test.tsx
@@ -1,11 +1,14 @@
 import { render } from '@testing-library/react';
 import { Heading } from './Heading';
 
-
 describe('Tests for Heading component', () => {
   test('should render a heading', () => {
-    render(<Heading level="h2" />)
+    render(<Heading level='h2' />);
   });
-  
+  test('should be less than 6 and greater than 0', () => {
+    const level = 3;
+    render(<Heading level={`h${level}`} />);
+    expect(level).toBeGreaterThan(0);
+    expect(level).toBeLessThan(7);
+  });
 });
-
