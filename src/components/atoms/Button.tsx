9f1797a96f1d610fa744a94a58f71aed655f0a7b
Dawid <dawtom97@gmail.com>
2022-11-28 18:22:29 +0100

    aaaaa

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,32 @@
+import { FC, MouseEvent, ReactNode } from 'react';
+import { Container } from './styles';
+
+export type ButtonProps = {
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
+  children?: ReactNode;
+  color?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'small' | 'regular' | 'large';
+  disabled?: boolean;
+  ariaLabel?: string;
+};
+
+export const Button: FC<ButtonProps> = ({
+  onClick,
+  children,
+  color = 'primary',
+  size = 'regular',
+  disabled,
+  ariaLabel = 'Press the button',
+}) => {
+  return (
+    <Container
+      size={size}
+      aria-label={ariaLabel}
+      onClick={onClick}
+      disabled={disabled}
+      color={color}
+    >
+      {children}
+    </Container>
+  );
+};
