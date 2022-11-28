443cea35523551c9bb6a3dce20f00033854156b8
Dawid <dawtom97@gmail.com>
2022-11-28 18:31:55 +0100

    restrucutre project folders

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { FC, MouseEvent, ReactNode } from 'react';
-import { Container } from './styles';
+import { Container } from './Button/styles';
 
 export type ButtonProps = {
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
--- src/components/atoms/Button/styles.ts
+++ src/components/atoms/Button/styles.ts
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import { ButtonProps } from './Button';
+import { ButtonProps } from '../Button';
 
 const COLOR = {
     primary: css`
